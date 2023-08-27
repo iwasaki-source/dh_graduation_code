f0d427eeb19fc6fae0baf93c4206d2569d86692a
Iwasaki Yudai <poettype73@gmail.com>
2023-08-27 23:48:49 +0900

    230826-27_スタジオ作業分

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ const swiper = new Swiper(".swiper", {
   },
   speed: 2000,
 });
+
+//TOPの撮影事例のスクロール
+
+const getScrollPercent = () => {
+  const scrolled = window.scrollY;
+  console.log(scrolled);
+  const percentage = ((scrolled - 3401) / (6139-3401)) * 100;
+  document.querySelector('.bar').style.height = `${percentage}%`;
+}
+
+window.addEventListener('scroll', getScrollPercent);
