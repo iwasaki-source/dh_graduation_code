b94c783656179cdf20833d41c1cf0f49d428bef2
Iwasaki Yudai <poettype73@gmail.com>
2023-08-28 23:49:46 +0900

    バーの修正

--- js/script.js
+++ js/script.js
@@ -11,11 +11,14 @@ const swiper = new Swiper(".swiper", {
 });
 
 //TOPの撮影事例のスクロール
+const elem = document.getElementById("index_example");
+const elemheight = elem.scrollHeight - 400;
+
+let elemtop = elem.getBoundingClientRect().top + window.pageYOffset;
 
 const getScrollPercent = () => {
   const scrolled = window.scrollY;
-  console.log(scrolled);
-  const percentage = ((scrolled - 3401) / (6139-3401)) * 100;
+  const percentage = ((scrolled - elemtop) / ((elemtop + elemheight) - elemtop)) * 100;
   document.querySelector('.bar').style.height = `${percentage}%`;
 }
 
